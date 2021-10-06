18bc22837584c679f1cbb5755216f1f69896000f
Karine MOREAU <kmoreau@cstj.qc.ca>
2021-10-06 07:59:37 -0400

    Fin du cours précédent

--- ProjetEnCours/Labo05Biblio.cpp
+++ ProjetEnCours/Labo05Biblio.cpp
@@ -1,4 +1,9 @@
 #include "Labo05Biblio.h"
+#include <iostream>
+using namespace std;
+
+
+
 double calculerMax(double valeur1, double valeur2, double valeur3)
 {
    // déclaration des variables
@@ -29,6 +34,74 @@ double calculerFactorielle(int nb)
    }
 
    return factorielle;
+}
+
+void afficherDateLongue(int j, int m, int a)
+{
+   cout << j;
+
+   switch (m)
+   {
+      case 1:
+         cout << " janvier ";
+         break;
+      case 2:
+         cout << " février ";
+         break;
+      case 3:
+         cout << " mars ";
+         break;
+      case 4:
+         cout << " avril ";
+         break;
+      case 5:
+         cout << " mai ";
+         break;
+      case 6:
+         cout << " juin ";
+         break;
+      case 7:
+         cout << " juillet ";
+         break;
+      case 8:
+         cout << " août ";
+         break;
+      case 9:
+         cout << " septembre ";
+         break;
+      case 10:
+         cout << " octobre ";
+         break;
+      case 11:
+         cout << " novembre ";
+         break;
+      case 12:
+         cout << " décembre ";
+         break;
+   }
+   cout << a;
+
+}
+
+bool etrePair(int nb)
+{
+   /*
+   if (nb % 2 == 0)
+   {
+      return true;
+   }
+   else
+   {
+   return false;
+
+   }
+   */
+
+   // return nb % 2 == 0;
+
+   return !(nb%2);
+
+
 }
 
 
--- ProjetEnCours/Labo05Biblio.h
+++ ProjetEnCours/Labo05Biblio.h
@@ -3,3 +3,9 @@
 double calculerMax(double valeur1, double valeur2, double valeur3);
 
 double calculerFactorielle(int nb);
+
+
+
+void afficherDateLongue(int j, int m, int a);
+
+bool etrePair(int nb);
